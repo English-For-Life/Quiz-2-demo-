59f8420b5a40c1d7bbd5063a0655d89ff8776ab0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-04-26 19:12:20 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,28 @@
+//Array of objects
+const quiz = [
+    {
+        q:"We ___ to the cinema last night.",
+        options:["went", "go", "are going"],
+        answer: 0
+    },
+    {
+        q:"It's ___ (9.45).",
+        options:["quarter past nine", "quarter to ten", "quarter to nine"],
+        answer: 1
+    },
+    {
+        q:"I'm from Poland, so I'm ___ .",
+        options:["Polanese", "Polac", "Polish"],
+        answer: 2
+    },
+    {
+        q:"Excuse me! ___ is this T-shirt?",
+        options:["How much", "How many", "How price"],
+        answer: 0
+    },
+    {
+        q:"I love Kate. She is so ___ .",
+        options:["annoying", "nice", "boring"],
+        answer: 1
+    }
+]
